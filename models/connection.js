3782cff904719abb1d15abbf7c15f3b951aaa728
skyehorrell <skyehorrell14@icloud.com>
2022-03-01 13:46:27 -0500

    added connection array

---
+++ models/connection.js
@@ -0,0 +1,22 @@
+const connections = [
+{
+    id: '1', 
+    title: 'Introduction Meeting',
+    content: 'Join us on Zoom to meet our team members and learn about our semester goals! Meeting ID: 353 213 3011', 
+    date: 'February 3, 2022', 
+    time: '1:00 pm - 2:00 pm', 
+    img: 'https://sites.uml.edu/vtr/files/2020/05/Zoom-Pro-Annually-2-1024x1024-1.jpg', 
+    imgAlt: 'Zoom logo'
+},
+{
+    id: '2', 
+    title: 'HTML Fundamentals Course', 
+    content: 'Join us to learn the fundamentals of HTML! This is the first step towards learning how to build your own website! Join on campus or on Zoom! Location on campus: Woodward 232. If joining on Zoom, meeting ID: 353 213 3011', 
+    date: 'February 16, 2022', 
+    time: '5:00 pm - 6:00 pm', 
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/HTML5_logo_black.svg/2048px-HTML5_logo_black.svg.png', 
+    imgAlt: 'HTML logo'
+}
+];
+
+console.log(connections[0]);

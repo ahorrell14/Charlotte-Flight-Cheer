f97ea643ad9b95ba0af368476e3d0cfb7e22f172
skyehorrell <skyehorrell14@icloud.com>
2022-03-01 13:31:07 -0500

    created connection routes

---
+++ controllers/connectionController.js
@@ -0,0 +1,34 @@
+//GET /connections: send all connections to the user
+exports.index = (req, res) => {
+    res.send('send all connections');
+};
+
+//GET /connections/new: send html form for creating a new connection
+exports.new = (req, res) => {
+    res.send('send the new form');
+};
+
+//POST /connections: create a new connection
+exports.create = (req, res) => {
+    res.send('created a new connection');
+};
+
+//GET /connections/:id: send details of connection identified by id
+exports.show = (req, res) => {
+    res.send('send connection with id ' + req.params.id);
+};
+
+//GET /connections/:id/edit: send html form for editing an existing connection
+exports.edit = (req, res) => {
+    res.send('send the edit form');
+};
+
+//PUT /connections/:id: update the connection identified by id
+exports.update = (req, res) => {
+    res.send('update connection with id ' + req.params.id);
+};
+
+//DELETE /connections/:id: delete the connection identified by id
+exports.delete = (req, res) => {
+    res.send('delete connection with id ' + req.params.id);
+};
--- routes/connectionRoutes.js
+++ routes/connectionRoutes.js
@@ -1,40 +1,27 @@
 const express = require('express');
+const controller = require('../controllers/connectionController');
 
 const router = express.Router();
 
 //GET /connections: send all connections to the user
-router.get('/', (req, res) => {
-    res.send('send all connections');
-});
+router.get('/', controller.index);
 
-//GET /connections/new: send html form for creating a new story
-router.get('/new', (req, res) => {
-    res.send('send the new form');
-});
+//GET /connections/new: send html form for creating a new connection
+router.get('/new', controller.new);
 
 //POST /connections: create a new connection
-router.post('/', (req, res) => {
-    res.send('created a new connection');
-});
+router.post('/', controller.create);
 
 //GET /connections/:id: send details of connection identified by id
-router.get('/:id', (req, res) => {
-    res.send('send connection with id ' + req.params.id);
-});
+router.get('/:id', controller.show);
 
-//GET /connections/:id/edit: send html form for editing an existing story
-router.get('/:id/edit', (req, res) => {
-    res.send('send the edit form');
-});
+//GET /connections/:id/edit: send html form for editing an existing connection
+router.get('/:id/edit', controller.edit);
 
 //PUT /connections/:id: update the connection identified by id
-router.put('/:id', (req, res) => {
-    res.send('update story with id ' + req.params.id);
-});
+router.put('/:id', controller.update);
 
-//DELETE /connections/:id: delete the story identified by id
-router.delete('/:id', (req, res) => {
-    res.send('delete story with id ' + req.params.id);
-});
+//DELETE /connections/:id: delete the connection identified by id
+router.delete('/:id', controller.delete);
 
 module.exports = router;
